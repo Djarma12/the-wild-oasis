cf5602ebab6097b704d9992e795eb341add2ec1a
Dusan Madjar <madjardusan@gmail.com>
2024-09-03 14:50:29 +0200

    Add Nested Routes And  Pages

--- the-wild-oasis-user/app/layout.js
+++ the-wild-oasis-user/app/layout.js
@@ -6,7 +6,7 @@ const josefin = Josefin_Sans({
   subsets: ["latin"],
   display: "swap",
 });
-console.log(josefin);
+
 export const metadata = {
   title: {
     template: "%s / The Wild Oasis",
@@ -20,8 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} antialiased bg-primary-950 relative text-primary-100 min-h-screen flex flex-col`}
-      >
+        className={`${josefin.className} antialiased bg-primary-950 relative text-primary-100 min-h-screen flex flex-col`}>
         <Header />
         <div className="flex-1 px-8 py-12">
           <main className="max-w-7xl mx-auto">{children}</main>
